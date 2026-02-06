dad6a729cbb44543192c1b2460e888c506dcec30
humbertoham <hamduranhumberto@gmail.com>
2026-02-06 09:47:34 -0600

    logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -37,8 +37,8 @@ export default function Nav() {
   return (
     <nav className="nav">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" className="flex pt-2 items-center space-x-3 rtl:space-x-reverse">
-          <Image width={100} height={50} src="/images/weblogo.svg" className="logo" alt="pink ponk logo" />
+        <Link href="/" className="flex pt-2 logoh items-center space-x-3 rtl:space-x-reverse">
+          Humberto Ham
         </Link>
         <button
           onClick={toggleMenu}
